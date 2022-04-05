7ca9769a0d5ba87d7c07d83c83b998a6e0fe8154
harryleventon <harry.leventon@gmail.com>
2022-04-05 11:57:20 +0000

    added some unit tests

--- application/routes.py
+++ application/routes.py
@@ -34,59 +34,48 @@ def addhouses():
     else:
         return render_template('addhouses.html', form=form, allhouses=allhouses)
 
-# @app.route('/clear')
-# def clear():
+@app.route('/delete_student/<name>')
+def delete_student(name):
+    student_to_delete = db.session.query(Students).filter_by(student_name=name).first()
+    if student_to_delete:
+        db.session.delete(student_to_delete)
+        db.session.commit()
+        return redirect(url_for('register'))
+    else:
+        return redirect(url_for('register'))
 
-#     allhouses = db.session.query(Houses).all()
+@app.route('/clearall')
+def clearall():
 
-#     for hous in allhouses:
-#         db.session.delete(hous)
-#         db.session.commit()
+    allstudents = db.session.query(Students).all()
+
+    for studs in allstudents:
+        db.session.delete(studs)
+        db.session.commit()
     
-#     return render_template('index.html', form=form, studentss=students)
+    return redirect(url_for('register'))
 
-# # @app.route('/read')
-# # def read():
-# #     all_houses = Houses.query.all()
-# #     houses_string = ""
-# #     for house in all_houses:
-# #         houses_string += "<br>"+ house.name
-# #     return houses_string
 
-# # @app.route('/update/<name>')
-# # def update(name):
-# #     first_house = Houses.query.first()
-# #     first_house.name = name
-# #     db.session.commit()
-# #     return first_house.name
+@app.route('/count_students')
+def count_students():
+    num_of_students = Students.query.count()
+    return f"The number of students in the database is {num_of_students}"
 
-# '''Add a new route called /delete that will delete the first entry in the database. Have it return a message to let you know you have deleted the entry.'''
 
-# # @app.route('/clear')
-# # def clear():
+@app.route('/update/<name>')
+def update(name):
+    first_student = Students.query.first()
+    first_student.student_name = name
+    db.session.commit()
+    return redirect(url_for('register'))
 
-# #     game_to_delete = Games.query.all()
-
-# #     for game in game_to_delete:
-# #         db.session.delete(game)
-# #         db.session.commit()
-# #         return "Games deleted"
-
-# # '''above for loop not quite working'''
 
 # # @app.route('/delete')
 # # def delete():
 # #     first_house = Houses.query.first()
 # #     db.session.delete(first_house)
-# #     db.session.commit()
+# #     db.session.co mmit()
 # #     return "First house deleted"
-
-# # '''Add another route that returns the number of houses in the database.'''
-
-# # @app.route('/count')
-# # def count():
-# #     list_of_houses = Houses.query.count()
-# #     return f"The number of houses in the database is {list_of_houses}"
 
 
 # '''--------------------------------------'''
---
+++ tests/test_app.py
@@ -0,0 +1,71 @@
+# Import the necessary modules
+from flask import url_for
+from flask_testing import TestCase
+
+# import the app's classes and objects
+from application import app, db
+from application.models import Students, Houses 
+
+# Create the base class
+class TestBase(TestCase):
+    def create_app(self):
+
+        # Pass in testing configurations for the app. 
+        app.config.update(SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:password@35.246.83.95:3306/flask_example_db',
+                SECRET_KEY='tangerines',
+                DEBUG=True,
+                WTF_CSRF_ENABLED=False
+                )
+        return app
+
+    # Will be called before every test
+    def setUp(self):
+        # Create table
+        db.drop_all()
+        db.create_all()
+        # Create test houses
+        for hous in ['red', 'yellow', 'green', 'blue']:
+            houses = Houses(house_name=hous)
+        # save houses to database
+            db.session.add(houses)
+        db.session.commit()
+
+        sample = {'Jessica':1,'Henry':2,'Giles':3,'Esme':4}
+        for stud in sample:
+            students = Students(student_name=stud,house_id=sample[stud])
+            db.session.add(students)
+        db.session.commit()
+
+    # Will be called after every test
+    def tearDown(self):
+        # Close the database session and remove all contents of the database
+        db.session.remove()
+        db.drop_all()
+
+# Write a test class to test Read functionality
+class TestRead(TestBase):
+    def test_index_get(self):
+        response = self.client.get(url_for('register'))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"red",response.data)
+        self.assertIn(b"yellow",response.data)
+        self.assertIn(b"blue",response.data)
+        self.assertIn(b"green",response.data)
+
+    def test_readHous(self):
+        response = self.client.get(url_for('addhouses'))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Add",response.data)
+
+# Write a test to test Post functionality i.e. adding a new student to the db or adding a new house to the db
+
+class TestAdd(TestBase):
+    def test_add_student(self):
+        response = self.client.post(url_for('register'), data = dict(name="TestStud",houseID=1), follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'TestStud', response.data)
+
+    def test_add_house(self):
+        response = self.client.post(url_for('addhouses'), data = dict(name="TestHouse"), follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'TestHouse', response.data)
