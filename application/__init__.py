8bef8e44cc75997bbd7e47b340e694ab4336c9bc
harryleventon <harry.leventon@gmail.com>
2022-04-07 15:51:01 +0000

    edited init

--- application/__init__.py
+++ application/__init__.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
+from os import getenv
 
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] =  "mysql+pymysql://root:password@35.246.83.95:3306/flask_example_db"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'tangerines' 
+app.config['SECRET_KEY'] = 'tangerines'
+# app.config['SECRET_KEY'] = str(os.getenv('SECRET_KEY'))
 
 db = SQLAlchemy(app)
 
